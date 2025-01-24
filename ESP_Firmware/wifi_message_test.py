23079d22e58873decc8d5053448b020870dfe98a
Prohurtz <48768484+RedHawk989@users.noreply.github.com>
2025-01-24 13:47:08 -0500

    update

--- ESP_Firmware/wifi_message_test.py
+++ ESP_Firmware/wifi_message_test.py
@@ -1,13 +1,12 @@
 import socket
 import random
-import time
 
 ESP_IP = "192.168.4.1"  # Default SoftAP IP of ESP32
 ESP_PORT = 80  # Port defined in the ESP32 code
 
 
 def send_command():
-    p1, p2, p3, p4, p5, p6 = [random.randint(0, 100) for _ in range(6)]
+    p1, p2, p3, p4, p5, p6 = 255, 255, 255, 255, 255, 255, #[random.randint(0, 100) for _ in range(6)]
     command = f"{p1} {p2} {p3} {p4} {p5} {p6}\n"
 
     try:

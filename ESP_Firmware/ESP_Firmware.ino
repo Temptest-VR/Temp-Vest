e9f01bd9da27d9db8b13526aea3640a6d6aae792
Prohurtz <48768484+RedHawk989@users.noreply.github.com>
2025-01-24 17:57:16 -0500

    thermistor POC example. Pending tuning

--- ESP_Firmware/ESP_Firmware.ino
+++ ESP_Firmware/ESP_Firmware.ino
@@ -33,7 +33,7 @@ const int peltier_4_reverse = 12;
 const int peltier_5_reverse = 27;
 const int peltier_6_reverse = 26;
 
-const int PWM_FREQ = 10;
+const int PWM_FREQ = 20;
 const int PWM_RESOLUTION = 8;
 const int max_power = 255; // Assuming the power range is 0-255
 
@@ -88,8 +88,12 @@ void setup() {
 }
 
 void run_peltiers(int p1_power, int p2_power, int p3_power, int p4_power, int p5_power, int p6_power) {
-    Serial.printf("Running Peltiers: P1=%d, P2=%d, P3=%d, P4=%d\n", p1_power, p2_power, p3_power, p4_power);
-    setPeltierPower(peltier_1_pin, p1_power);
+    Serial.printf("Running Peltiers: P1=%d, P2=%d, P3=%d, P4=%d, P5=%d, P6=%d\n", p1_power, p2_power, p3_power, p4_power, p5_power, p6_power);
+
+    if !String(analogRead(therm1_pin)) < 1300 || String(analogRead(therm1_pin)) > 1800) { // placeholder values. NEED TO CALIBRATE!!!
+      setPeltierPower(peltier_1_pin, p1_power);
+    } // lower as heat up
+    
     setPeltierPower(peltier_2_pin, p2_power);
     setPeltierPower(peltier_3_pin, p3_power);
     setPeltierPower(peltier_4_pin, p4_power);
@@ -113,6 +117,7 @@ void run_peltiers(int p1_power, int p2_power, int p3_power, int p4_power, int p5
 
 void setPeltierPower(int pin, int power) {
     int duty_cycle = map(abs(power), 0, 100, 0, 255);
+
     Serial.printf("Setting pin %d to duty cycle %d\n", pin, duty_cycle);
     analogWrite(pin, duty_cycle);
     if (power < 0) {
